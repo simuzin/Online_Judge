fd5a92fd1ad4912cf4efd213264e08c119666f37
simuzin <128016419+simuzin@users.noreply.github.com>
2024-01-30 12:57:23 +0900

    [Silver IV] Title: 카드2, Time: 168 ms, Memory: 55036 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.py
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -1,11 +1,10 @@
 import sys
 from collections import deque
-
-n = int(sys.stdin.readline())
-temp = deque([i for i in range(1, n+1)])
+n = int(sys.stdin.readline().strip())
+card = deque([i for i in range(1,n+1)])
 while True:
-    if len(temp) == 1:
+    if len(card) == 1:
         break
-    temp.popleft()
-    temp.rotate(-1)
-print(temp.pop())
+    card.popleft()
+    card.rotate(-1)
+print(card[0])

55f9d6faa5c2ec48f7e93fac70c35bcd7bb4d61b
simuzin <128016419+simuzin@users.noreply.github.com>
2024-03-27 13:07:12 +0900

    [Silver II] Title: 최대 힙, Time: 128 ms, Memory: 37044 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline().strip())
+priority_queue = []
+
+for i in range(n):
+    key = int(sys.stdin.readline().strip())
+    if key == 0:
+        if len(priority_queue) == 0:
+            print("0")
+        else:
+            temp = -(heapq.heappop(priority_queue))
+            print(temp)
+    else:
+        heapq.heappush(priority_queue, -key)

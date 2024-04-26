e9d5e1b03ea5ef57603899938c4c8a8b28902e49
simuzin <128016419+simuzin@users.noreply.github.com>
2024-04-26 10:46:09 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -0,0 +1,8 @@
+from collections import deque
+def solution(numbers, direction):
+    answer = deque(numbers)
+    if direction == 'right':
+        answer.rotate(1)
+    else:
+        answer.rotate(-1)
+    return list(answer)

d79c3799964ff228e5873d7c83cc76032a01fb22
simuzin <128016419+simuzin@users.noreply.github.com>
2024-10-15 16:20:56 +0900

    [Silver I] Title: 안전 영역, Time: 588 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,42 +1,35 @@
 import sys
+from collections import deque
 
 n = int(sys.stdin.readline().strip())
-height, location = [], []
-for i in range(n):
-    temp = list(map(int, sys.stdin.readline().strip().split()))
-    location.append(temp)
-    height.extend(set(temp))
-height = sorted(list(set(height)))
+board = [list(map(int, sys.stdin.readline().strip().split())) for _ in range(n)]
 
-dirY = [-1,0,1,0]
-dirX = [0,1,0,-1]
-res =[]
-for h in height:
-    maps, cnt = [], 0
-    queue, isVisit = [], [[False for i in range(n)] for j in range(n)]
-    for i in range(n):
-        maps.append([])
-        for j in range(n):
-            if location[i][j] <= h:
-                maps[-1].append(0)
-            else:
-                maps[-1].append(1)
-    for i in range(n):
-        for j in range(n):
-            if maps[i][j] == 1 and isVisit[i][j] == False:
-                queue.append([i,j])
-                isVisit[i][j] = True
+flat_board = sum(board,[])
+min_height, max_height = min(flat_board), max(flat_board),
+
+def bfs(water):
+    visited_bfs = [[False] * n for _ in range(n)]
+
+    queue = deque([])
+    dir_y, dir_x = [1, 0, -1, 0], [0, 1, 0, -1]
+    cnt = 0
+    for j in range(n):
+        for i in range(n):
+            if board[j][i] > water and not visited_bfs[j][i]:
+                queue.append((j, i))
+                visited_bfs[j][i] = True
                 cnt += 1
                 while queue:
-                    y,x = queue.pop(0)
-                    for k in range(4):
-                        NextY = y + dirY[k]
-                        NextX = x + dirX[k]
-                        if (0<=NextY<n and 0<=NextX<n) and maps[NextY][NextX] == 1 and isVisit[NextY][NextX] == False:
-                            queue.append([NextY, NextX])
-                            isVisit[NextY][NextX] = True
-                res.append(cnt)
-if res:
-    print(max(res))
-else:
-    print(1)
+                    y, x = queue.popleft()
+                    for i in range(4):
+                        next_y = y + dir_y[i]
+                        next_x = x + dir_x[i]
+                        if next_y >= 0 and next_x >= 0 and next_y < n and next_x<n and board[next_y][next_x] > water and not visited_bfs[next_y][next_x]:
+                            queue.append((next_y, next_x))
+                            visited_bfs[next_y][next_x] = True
+    return cnt
+
+max_cnt = 1
+for i in range(min_height, max_height):
+    max_cnt = max(max_cnt, bfs(i))
+print(max_cnt)

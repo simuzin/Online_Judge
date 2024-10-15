4e1bc4bcf12e6fcf80a4f4fd910bf7467a9f647f
simuzin <128016419+simuzin@users.noreply.github.com>
2024-10-15 15:23:03 +0900

    [Silver II] Title: DFS와 BFS, Time: 160 ms, Memory: 40412 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -2,36 +2,42 @@ import sys
 from collections import deque
 
 n, m, v = map(int, sys.stdin.readline().strip().split())
-board = [[False for _ in range(n+1)] for _ in range(n+1)]
-is_visit_bfs = [False for _ in range(n+1)]
-is_visit_dfs = [False for _ in range(n+1)]
+graph = [[False for _ in range(n+1)] for _ in range(n+1)]
+visited_dfs = [False for _ in range(n+1)]
+visited_bfs = [False for _ in range(n+1)]
 
 for _ in range(m):
-    first, second = map(int, sys.stdin.readline().strip().split())
-    board[first][second] = board[second][first]= True
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a][b] = graph[b][a] = True
 
-# DFS 구현
-stack = [v]
+# DFS 구현 (재귀 없이 스택 사용)
+def dfs(start):
+    stack = [start]
+    while stack:
+        node = stack.pop()
+        if not visited_dfs[node]:
+            visited_dfs[node] = True
+            print(node, end = ' ')
 
-while stack:
-    num = stack.pop()
-    if not is_visit_dfs[num]:
-        is_visit_dfs[num] = True
-        print(num, end = ' ')
+            # 작은 번호부터 방문하기 위해, 역순
+            for i in range(n, 0, -1):
+                if graph[node][i] and not visited_dfs[i]:
+                    stack.append(i)
 
-        for i in range(n, 0, -1):
-            if board[num][i] and not is_visit_dfs[i]:
-                stack.append(i)
-            
-print('')
-# BFS 구현
-queue = deque([v])
-is_visit_bfs[v] = True
-while queue:
-    num = queue.popleft()
-    print(num, end=' ')
+# BFS 구현 (큐 사용)
+def bfs(start):
+    queue = deque([start])
+    visited_bfs[start] = True
 
-    for i in range(1, n+1):
-        if board[num][i] and not is_visit_bfs[i]:
-            queue.append(i)
-            is_visit_bfs[i] = True
+    while queue:
+        node = queue.popleft()
+        print(node, end=' ')
+
+        for i in range(1, n+1):
+            if graph[node][i] and not visited_bfs[i]:
+                queue.append(i)
+                visited_bfs[i] = True
+
+dfs(v)
+print()
+bfs(v)

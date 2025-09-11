744c6bd80b4389a16e86ac2b417ffe7ce7b9d7a6
simuzin <128016419+simuzin@users.noreply.github.com>
2025-09-12 01:36:36 +0900

    [Silver I] Title: 단지번호붙이기, Time: 36 ms, Memory: 32488 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,39 +1,35 @@
 import sys
 
-n = int(sys.stdin.readline().strip())
-ground = [[0 for i in range(n)] for j in range(n)]
-for i in range(n):
-    s = sys.stdin.readline().strip()
-    for j in range(n):
-        ground[i][j] = int(s[j])
+input = sys.stdin.readline
 
-# 시계 방향으로 순회 (상, 우, 하, 좌)
-dirY = [-1,0,1,0]
-dirX = [0,1,0,-1]
-queue, home =[], [[0 for i in range(n)] for j in range(n)]
+N = int(input().strip())
+board = [list(map(int, input().strip())) for _ in range(N)]
+visited = [[False] * N for _ in range(N)]
+
+dir_y, dir_x = [1,0,-1,0],[0,1,0,-1]
+result = []
 cnt = 0
 
-for i in range(n):
-    for j in range(n):
-        if ground[i][j] == 1 and home[i][j] == 0:
-            queue.append([i,j])
-            cnt += 1
-            home[i][j] = cnt
-            while queue:
-                y,x = queue.pop(0)
-                for k in range(4):
-                    nextY = y + dirY[k]
-                    nextX = x + dirX[k]
-                    if (nextY < n and nextY >= 0 and nextX < n and nextX >= 0) and (ground[nextY][nextX] == 1) and (home[nextY][nextX] == 0):
-                        queue.append([nextY, nextX])
-                        home[nextY][nextX] = cnt
+def dfs(j,i):
+    global cnt
+    cnt += 1
+    for k in range(4):
+        y = j + dir_y[k]
+        x = i + dir_x[k]
+        if 0<=y<N and 0<=x<N and board[y][x]==1 and not visited[y][x]:
+            visited[y][x] = True
+            dfs(y,x)
 
-# 출력
-print(cnt)
-res = [0 for i in range(cnt+1)]
-for k in range(n):
-    for i in range(1,cnt+1):
-        res[i] += home[k].count(i)
-res.sort()
-for i in range(1, cnt+1):
-    print(res[i])
+
+for j in range(N):
+    for i in range(N):
+        if board[j][i]==1 and not visited[j][i]:
+            visited[j][i] = True
+            cnt = 0
+            dfs(j,i)
+            result.append(cnt)
+
+result.sort()
+print(len(result))
+for res in result:
+    print(res)

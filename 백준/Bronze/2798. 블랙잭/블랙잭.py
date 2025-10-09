988a7dbb2dc70816e845d7338e775ddbd9ba5aa2
simuzin <128016419+simuzin@users.noreply.github.com>
2025-10-09 21:49:05 +0900

    [Bronze II] Title: 블랙잭, Time: 60 ms, Memory: 38784 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,11 +1,18 @@
 import sys
-from itertools import combinations
+input = sys.stdin.readline
 
-_,m = map(int, sys.stdin.readline().split())
-temp = list(map(int, sys.stdin.readline().split()))
-data = {}
-for comb in combinations(temp,3):
-    if sum(comb) <= m:
-        data[abs(m-sum(comb))] = sum(comb)
-res = sorted(data.items(), key = lambda x:x[0])
-print(res[0][1])
+N, M = map(int, input().split())
+cards = list(map(int, input().split()))
+
+result = []
+
+def recur(start, depth, sum):
+    if depth == 3:
+        if sum <= M:
+            result.append(sum)
+        return
+    for i in range(start, len(cards)):
+        recur(i+1, depth+1, sum + cards[i])
+
+recur(0,0,0)
+print(max(result))
